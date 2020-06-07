eb53f68144917f80b464d77ef48ebc65528d7afd
Kirill Urazmanov <urazmanov@vbankcenter.ru>
2020-06-07 11:20:12 +0300

    Промежуточный комит

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { get } from 'http';
 import { mkdirSync, existsSync, writeFile } from 'fs';
 const kebabCase = require('lodash.kebabcase');
 
-enum Types {
-  String = 'string',
-  Integer = 'integer',
-  Number = 'number',
-  Boolean = 'boolean',
-  Array = 'array',
-  Object = 'object',
-}
+const TYPES = {
+  string: 'string',
+  integer: 'integer',
+  number: 'number',
+  boolean: 'boolean',
+  array: 'array',
+  object: 'object',
+};
 
 const DEFAULTS = {
   swaggerURL: 'http://localhost:8080/v2/api-docs',
@@ -19,7 +19,6 @@ export class DikiyParser {
   async init() {
     const swaggerResponse = await this.createSwaggerRequest();
     const { definitions } = JSON.parse(JSON.parse(String(swaggerResponse)));
-
     for (const def of Object.values<any>(definitions)) {
       const fileName = def.title;
       const fileData = this.createFileData(def);
@@ -27,7 +26,7 @@ export class DikiyParser {
     }
   }
 
-  private createSwaggerRequest(): Promise<any> {
+  private createSwaggerRequest(): Promise<string> {
     return new Promise((resolve, reject) => {
       get(DEFAULTS.swaggerURL, res => {
         let data = '';
@@ -39,22 +38,25 @@ export class DikiyParser {
     });
   }
 
-  private createFileData(def: { [k: string]: any }, deps: string[] = []) {
+  private createFileData(def: { [k: string]: any }) {
     if (def.properties === undefined) {
       return '';
     }
+    const deps: string[] = [];
     const dtoName = def.title;
     let data = `export interface ${dtoName}  {\r\n`;
     Object.entries<any>(def.properties).forEach(([propName, propValue]) => {
       const { type } = propValue;
 
-      if (type == Types.String) {
-        data += `  ${propName}: string;`;
-      } else if (type == Types.Integer || type === Types.Number) {
-        data += `  ${propName}: number;`;
-      } else if (type == Types.Boolean) {
-        data += `  ${propName}: boolean;`;
-      } else if (type == Types.Array) {
+      if (type == TYPES.string) {
+        data += `  ${propName}: ${TYPES.string};`;
+      } else if (type == TYPES.integer || type === TYPES.number) {
+        data += `  ${propName}: ${TYPES.number};`;
+      } else if (type == TYPES.boolean) {
+        data += `  ${propName}: ${TYPES.boolean};`;
+      } else if (type == TYPES.object) {
+        data += `  ${propName}: ${this.createObjectAdditionalData(propValue, data, deps)};`;
+      } else if (type == TYPES.array) {
         data += `  ${propName}: any[];`;
       } else if (type === undefined && typeof propValue.$ref === 'string') {
         const dtoName = propValue.$ref.replace('#/definitions/', '');
@@ -72,8 +74,22 @@ export class DikiyParser {
     return data + '}';
   }
 
-  private createInterfaceProps(){
-    let data = ''
+  private createProp(){
+    let data
+  }
+
+  private createObjectAdditionalData(propValue: { [k: string]: any }, deps: string[] = []): string {
+    let data = '';
+    if (propValue.additionalProperties) {
+      const { type } = propValue.additionalProperties;
+      if (type === TYPES.string) {
+        data += `{ [k: string]: ${TYPES.string} }`;
+      } else if (TYPES.integer || type === TYPES.number) {
+        data += `{ [k: string]: ${TYPES.number} }`;
+      }
+    }
+
+    return data;
   }
 
   private saveFile(dtoName: string, data: string) {
